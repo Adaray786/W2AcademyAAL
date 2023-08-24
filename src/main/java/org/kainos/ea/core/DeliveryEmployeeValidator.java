26d7ff43fffb3b7d5e9a10cba20ee9fb03a8333d
Leonardo Bishop <me@leonardobishop.com>
2023-08-24 12:13:54 +0100

    Add validator for delivery employee reqeust

--- src/main/java/org/kainos/ea/core/DeliveryEmployeeValidator.java
+++ src/main/java/org/kainos/ea/core/DeliveryEmployeeValidator.java
@@ -4,7 +4,21 @@ import org.kainos.ea.cli.DeliveryEmployeeRequest;
 
 public class DeliveryEmployeeValidator {
     public String isValidDeliveryEmployee(DeliveryEmployeeRequest request) {
-        //TODO validate delivery employee
+        if (request.getName().length() > 70) {
+            return "Name must be less than or equal to 70 characters";
+        }
+
+        if (request.getBankAccountNumber().length() > 8) {
+            return "Bank account number must be less than or equal to 8 characters";
+        }
+
+        if (request.getNiNumber().length() != 9) {
+            return "National Insurance number must be 9 characters";
+        }
+
+        if (request.getSalary() < 0) {
+            return "Salary cannot be negative";
+        }
 
         return null;
     }
