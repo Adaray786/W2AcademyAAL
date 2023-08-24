59db28e722242e30da4cb6edfe516302c9f95380
Adam Uddin <adamuddin@outlook.com>
2023-08-24 10:36:16 +0100

    adding Relevant IDs to Sales and Delivery

--- src/main/java/org/kainos/ea/cli/SalesEmployee.java
+++ src/main/java/org/kainos/ea/cli/SalesEmployee.java
@@ -1,11 +1,22 @@
 package org.kainos.ea.cli;
 
 public class SalesEmployee extends Employee{
-    public SalesEmployee(int employeeID, double salary, String bankAccountNumber, String NINumber, float comissionRate) {
+    public SalesEmployee(int employeeID, double salary, String bankAccountNumber, String NINumber, float comissionRate, int salesEmployeeID) {
         super(employeeID, salary, bankAccountNumber, NINumber);
         setComissionRate(comissionRate);
+        setSalesEmployeeID(salesEmployeeID);
     }
     private float comissionRate;
+
+    private int salesEmployeeID;
+
+    public int getSalesEmployeeID() {
+        return salesEmployeeID;
+    }
+
+    public void setSalesEmployeeID(int salesEmployeeID) {
+        this.salesEmployeeID = salesEmployeeID;
+    }
 
     public float getComissionRate() {
         return comissionRate;
